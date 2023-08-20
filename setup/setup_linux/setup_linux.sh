ae1c68a4f1aba381b2ec78b5a7ba3e9d85b45376
kart <karthik.panchap@gmail.com>
2023-08-19 19:43:22 -0500

    .

---
+++ setup/setup_linux/setup_linux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo cd ~/
+
+# SETUP : Zsh
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install zsh git-core curl fonts-powerline -y
+sudo chsh -s $(which zsh)
+
+# SETUP : Oh-my-zsh : https://ohmyz.sh/#install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# INSTALL : oh-my-zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions         ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions             ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+# INSTALL : p10k plugin
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git   ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Interactive!
+p10k configure
